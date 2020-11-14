54f5da8e1fa4cfb7cc37184b20b885d240ae5210
Gian Cortez <gcortez3450@gmail.com>
2020-11-14 22:27:25 +0800

    Changed the list into <f:selectOneMenu> and added getters and setters in requestOrderFormBean

--- techSavvy-JSF/src/main/java/apc/entjava/techSavvy/requestOrderFormBean.java
+++ techSavvy-JSF/src/main/java/apc/entjava/techSavvy/requestOrderFormBean.java
@@ -1,4 +1,76 @@
 package apc.entjava.techSavvy;
 
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@ManagedBean
+@RequestScoped
+@Named
 public class requestOrderFormBean {
+    private String pcModel, pcOS, pcProcessor, pcGraphics, pcMemory, pcStorage, pcDisplay, pcWarranty;
+
+    public String getPcModel() {
+        return pcModel;
+    }
+
+    public void setPcModel(String pcModel) {
+        this.pcModel = pcModel;
+    }
+
+    public String getPcOS() {
+        return pcOS;
+    }
+
+    public void setPcOS(String pcOS) {
+        this.pcOS = pcOS;
+    }
+
+    public String getPcProcessor() {
+        return pcProcessor;
+    }
+
+    public void setPcProcessor(String pcProcessor) {
+        this.pcProcessor = pcProcessor;
+    }
+
+    public String getPcGraphics() {
+        return pcGraphics;
+    }
+
+    public void setPcGraphics(String pcGraphics) {
+        this.pcGraphics = pcGraphics;
+    }
+
+    public String getPcMemory() {
+        return pcMemory;
+    }
+
+    public void setPcMemory(String pcMemory) {
+        this.pcMemory = pcMemory;
+    }
+
+    public String getPcStorage() {
+        return pcStorage;
+    }
+
+    public void setPcStorage(String pcStorage) {
+        this.pcStorage = pcStorage;
+    }
+
+    public String getPcDisplay() {
+        return pcDisplay;
+    }
+
+    public void setPcDisplay(String pcDisplay) {
+        this.pcDisplay = pcDisplay;
+    }
+
+    public String getPcWarranty() {
+        return pcWarranty;
+    }
+
+    public void setPcWarranty(String pcWarranty) {
+        this.pcWarranty = pcWarranty;
+    }
 }
